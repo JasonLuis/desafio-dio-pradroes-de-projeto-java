9d9a0f61eaeca4fea61123c057c27d19c855242a
JasonLuis <jason20luis@gmail.com>
2023-05-30 12:26:13 -0300

    chore: replicate the challenge project exploring design patterns in practice with java

---
+++ src/main/java/one/digitalinnovation/gof/LabPadroesProjetoSpringApplication.java
@@ -0,0 +1,17 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.cloud.openfeign.FeignAutoConfiguration;
+
+@EnableFeignClients
+@SpringBootApplication
+public class LabPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+	}
+
+}
